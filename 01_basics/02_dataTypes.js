18402e2562c0c3d32e007010c8a517e5089e1b9c
Milan Soni <123074437+Iammilansoni@users.noreply.github.com>
2024-07-17 14:52:40 +0000

    container commit done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict "; //treat all JS code as newer version
+
+//alert (3=3) //we are using node.js, not browser
+
+console.log(3+3) //code redeability should be high it's a correct way 
+
+console.log("Milan");
