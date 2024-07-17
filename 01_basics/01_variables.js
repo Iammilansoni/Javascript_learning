bb9da20c9c56af452b23569615e5f00d6a64da1f
Milan Soni <123074437+Iammilansoni@users.noreply.github.com>
2024-07-17 14:30:48 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14453
+let accountEmail = "hitesh@gmail.com"
+var accountPassword ="123456"
+accountCity = "Jaipur"
+
+// accountId = 2 //not allowed 
+
+accountEmail="hc@hc.com"
+accountPassword ="212121"
+accountCity = "Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
